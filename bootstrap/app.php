eab6681cf0329b3c1d58d601e96200e5c8429736
ZIMRION <zimrionvarethos@gmail.com>
2026-05-04 20:51:59 +0700

    push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
+        $middleware->trustProxies(at: '*');
+    
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'checkrole' => \App\Http\Middleware\CheckRole::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
